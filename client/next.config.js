a01574f1ba18e9bddb025b24087bb859949b9fcb
Marcos Ewbank <marcos.suarez@pivotree.com>
2023-09-18 21:58:06 +0100

    Add missing devDependencies

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nodeExternals = require("webpack-node-externals");
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = {
+  ...nextConfig,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate",
+        "supports-color": "supports-color",
+      });
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
